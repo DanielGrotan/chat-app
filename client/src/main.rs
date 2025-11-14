a20828d395d1129d9d6ea421b9bf16400ece3a95
Daniel Grøtan Gregusson <danielgrotangregusson@gmail.com>
2025-11-14 18:47:39 +0100

    feat: add client test script

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use common::protocol::{ClientMessage, ServerMessage, read_msg, write_msg};
+use tokio::net::TcpStream;
+
+#[tokio::main]
+async fn main() -> tokio::io::Result<()> {
+    let mut socket = TcpStream::connect("localhost:8080").await?;
+
+    let msg = ClientMessage::JoinRequest {
+        username: "My username".to_string(),
+    };
+    write_msg(&mut socket, &msg).await?;
+
+    let msg = ClientMessage::Chat {
+        text: "Hello from CLI test!".into(),
+    };
+    write_msg(&mut socket, &msg).await?;
+
+    loop {
+        match read_msg::<_, ServerMessage>(&mut socket).await {
+            Ok(msg) => println!("Received: {:?}", msg),
+            Err(e) => eprintln!("Error reading message: {:?}", e),
+        }
+    }
 }
